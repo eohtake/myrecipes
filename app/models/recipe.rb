221b83d04a6d52bc4faab0bd06031576daeea61c
Eric Ohtake <ericevangelista@gmail.com>
2015-11-16 23:55:17 +0900

    added image uploader

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,18 +1,22 @@
 class Recipe < ActiveRecord::Base
   belongs_to :chef
 
-  validates :chef_id,
-            presence: true
+  validates :chef_id, presence: true
 
-  validates :name,
-            presence: true,
-            length: { minimum: 5, maximum: 100 }
+  validates :name, presence: true, length: { minimum: 5, maximum: 100 }
 
-  validates :summary,
-            presence: true,
-            length: { minimum: 10, maximum: 150 }
+  validates :summary, presence: true, length: { minimum: 10, maximum: 150 }
 
-  validates :description,
-            presence: true,
-            length: { minimum: 20, maximum: 500 }
+  validates :description, presence: true, length: { minimum: 20, maximum: 500 }
+
+  mount_uploader :picture, PictureUploader
+
+  validate :picture_size
+
+  private
+    def picture_size
+      if picture.size > 5.megabytes
+        error.add(:picture, "should be smaller than 5MB")
+      end
+    end
 end
